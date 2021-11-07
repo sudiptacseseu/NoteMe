6b70224edfa1824d5ea8886a6ec72e4f37047da0
Sudipta Karmakar <sudiptacse.seu@gmail.com>
2021-11-07 18:42:11 +0600

    Test fragment implementation done

--- app/src/main/java/com/sudiptacseseu/noteme/fragment/TestFragment.java
+++ app/src/main/java/com/sudiptacseseu/noteme/fragment/TestFragment.java
@@ -1,25 +1,77 @@
 package com.sudiptacseseu.noteme.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.sudiptacseseu.noteme.R;
+import com.sudiptacseseu.noteme.activity.NoteDetailsActivity;
+import com.sudiptacseseu.noteme.adapter.NoteListAdapter;
+import com.sudiptacseseu.noteme.databinding.FragmentOpenBinding;
+import com.sudiptacseseu.noteme.model.Note;
+import com.sudiptacseseu.noteme.utils.OnItemClickListener;
+import com.sudiptacseseu.noteme.viewmodel.ToDoViewModel;
 
-public class TestFragment extends Fragment {
+import java.util.List;
+
+public class TestFragment extends Fragment implements OnItemClickListener {
 
     public TestFragment() {
         // Required empty public constructor
     }
 
+    ToDoViewModel toDoViewModel;
+    private NoteListAdapter noteListAdapter;
+    FragmentOpenBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_open, container, false);
+        toDoViewModel = new ViewModelProvider(this).get(ToDoViewModel.class);
+        View view = binding.getRoot();
+
+        noteListAdapter = new NoteListAdapter(getContext(), this);
+        binding.recyclerview.setAdapter(noteListAdapter);
+        noteListAdapter.notifyDataSetChanged();
+        binding.recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        toDoViewModel.getToDosByStatus("Test").observe(getViewLifecycleOwner(), new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+
+                Log.d("length", String.valueOf(notes.size()));
+                //update the cached copy of todos in the adapter
+                if (notes != null && notes.size()!= 0) {
+                    noteListAdapter.setToDos(notes);
+                    binding.noTaskTextViewId.setVisibility(View.INVISIBLE);
+                } else {
+                    binding.noTaskTextViewId.setText(R.string.no_note);
+                }
+            }
+        });
+
+        return view;
     }
+    @Override
+    public void onItemClick(Note note) {
+        Intent intent = new Intent(getContext(), NoteDetailsActivity.class);
+        intent.putExtra("currentClickedNote", note);
+        startActivity(intent);
+
+    }
+
 }
